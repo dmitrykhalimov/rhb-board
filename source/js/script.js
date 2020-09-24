f93eb9cdd83e7eca41688fb209cc0a2c1f022be1
Bluesman <dmitry.khalimov@gmail.com>
2020-09-24 12:31:39 +0300

    Добавляет возможность сворачивания разделов

--- source/js/script.js
+++ source/js/script.js
@@ -17,6 +17,20 @@ else {
 }
 });
 
+var buttonsExpand = document.querySelectorAll('.button-expand');
+console.log(buttonsExpand);
+
+var handleExpand = function(evt) {
+  if (evt.target.classList.contains(OPENED_BUTTON)) {
+    evt.target.classList.remove(OPENED_BUTTON);
+  } else {
+    evt.target.classList.add(OPENED_BUTTON);
+  }
+}
+
+buttonsExpand.forEach(button => {
+  button.addEventListener('click', handleExpand);
+});
 // var buttonBefore = document.querySelector('.example__catchange-text--before');
 // var buttonAfter = document.querySelector('.example__catchange-text--after');
 
