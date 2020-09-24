b24259ddcc9e02a44ae0925b583d91309179b891
Bluesman <dmitry.khalimov@gmail.com>
2020-09-24 11:38:21 +0300

    Реализует мобильную версию главной страницы + меню гамбургер

--- source/js/script.js
+++ source/js/script.js
@@ -1,35 +1,35 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle-menu');
+var navMain = document.querySelector('.page__nav');
+var navToggle = document.querySelector('.hamburger');
 
-var openedClass = 'main-nav--opened';
-var closedClass = 'main-nav--closed';
+var OPENED_BUTTON = 'is-active';
+var OPENED_NAV = 'page__nav--is-active'
 
-navMain.classList.remove('main-nav--nojs');
+// navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-if (navMain.classList.contains(closedClass)) {
-navMain.classList.remove(closedClass);
-navMain.classList.add(openedClass);
+if (navToggle.classList.contains(OPENED_BUTTON)) {
+  navToggle.classList.remove(OPENED_BUTTON)
+  navMain.classList.remove(OPENED_NAV);
 }
 else {
-navMain.classList.remove(openedClass);
-navMain.classList.add(closedClass);
+  navToggle.classList.add(OPENED_BUTTON)
+  navMain.classList.add(OPENED_NAV)
 }
 });
 
-var buttonBefore = document.querySelector('.example__catchange-text--before');
-var buttonAfter = document.querySelector('.example__catchange-text--after');
+// var buttonBefore = document.querySelector('.example__catchange-text--before');
+// var buttonAfter = document.querySelector('.example__catchange-text--after');
 
-var pictureBefore = document.querySelector('.example__cat-before');
-var pictureAfter = document.querySelector('.example__cat-after');
+// var pictureBefore = document.querySelector('.example__cat-before');
+// var pictureAfter = document.querySelector('.example__cat-after');
 
-var hiddenClass = 'visually-hidden';
+// var hiddenClass = 'visually-hidden';
 
-buttonBefore.addEventListener('click', function() {
-  pictureAfter.classList.add(hiddenClass);
-  pictureBefore.classList.remove(hiddenClass);
-});
-buttonAfter.addEventListener('click', function() {
-  pictureBefore.classList.add(hiddenClass);
-  pictureAfter.classList.remove(hiddenClass);
-});
+// buttonBefore.addEventListener('click', function() {
+//   pictureAfter.classList.add(hiddenClass);
+//   pictureBefore.classList.remove(hiddenClass);
+// });
+// buttonAfter.addEventListener('click', function() {
+//   pictureBefore.classList.add(hiddenClass);
+//   pictureAfter.classList.remove(hiddenClass);
+// });
